370b438dcecc2f19168f8636423b95f450711103
Sajid Hasan <sajid@kelda.no>
2024-07-30 09:53:22 +0600

    322. Refactoring the Project Structure

--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,21 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { songsReducer, addSong, removeSong } from "./slices/songsSlice";
+import { moviesReducer, addMovie, removeMovie } from "./slices/moviesSlice";
+import { reset } from "./actions";
+
+const store = configureStore({
+  reducer: {
+    songs: songsReducer,
+    movies: moviesReducer
+  }
+});
+
+export { store, reset, addSong, removeSong, addMovie, removeMovie };
+
+
+// Previous Code (Before formatting)
+/*
+
 import { configureStore, createSlice, createAction } from '@reduxjs/toolkit'
 
 export const reset = createAction("app/reset");
@@ -83,3 +101,5 @@ console.log(finalState)
 export { store };
 export const { addSong, removeSong } = songsSlice.actions;
 export const { addMovie, removeMovie } = moviesSlice.actions;
+
+*/
