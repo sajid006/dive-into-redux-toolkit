0febb4d13885697e7a4ea69197eda3d0d6a81c1d
Sajid Hasan <sajid@kelda.no>
2024-08-10 17:23:30 +0600

    393 Changing data with mutations

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,6 +1,38 @@
+import { useFetchAlbumsQuery, useAddAlbumMutation } from "../store";
+import ExpandablePanel from "./ExpandedPanel";
+import Button from "./Button";
+
 function AlbumsList({ user }) {
-    return <div>Albums for {user.name}</div>;
+  const { data, error, isLoading } = useFetchAlbumsQuery(user);
+  const [ addAlbum, results] = useAddAlbumMutation();
+
+  const handleAddAlbum = () => {
+    addAlbum(user);
   }
-  
-  export default AlbumsList;
-  
+
+  let content;
+  if (isLoading) {
+    content = <div>Loading...</div>;
+  } else if (error) {
+    content = <div>Error loading albums</div>;
+  } else {
+    content = data.map((album) => {
+      const header = <div>{album.title}</div>;
+      return (
+        <ExpandablePanel key={album.id} header={header}>
+          List of photos in the album
+        </ExpandablePanel>
+      );
+    });
+  }
+  return (
+    <>
+      <div>Albums for {user.name}
+        <Button onClick={handleAddAlbum}>+Add Album</Button>
+      </div>
+      {content}
+    </>
+  );
+}
+
+export default AlbumsList;
